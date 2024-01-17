4275210858e26e2364c7dd5e53002c1e21d1517e
Jeeho Ahn <io@jcde.xyz>
2024-01-17 13:46:19 +0900

    Update dialog-copy.tsx

--- app/@modal/dialog-copy.tsx
+++ app/@modal/dialog-copy.tsx
@@ -58,7 +58,7 @@ const DialogContent = React.forwardRef<
             "absolute right-4 top-4 rounded-sm opacity-70 ring-offset-background transition-opacity hover:opacity-100 focus:outline-none focus:ring-2 focus:ring-ring focus:ring-offset-2 disabled:pointer-events-none data-[state=open]:bg-accent data-[state=open]:text-muted-foreground"
           )}
           onAnimationEnd={(e) => {
-          if (e.animationName === "exit") router.back();
+            router.back();
         }}
           
         >
