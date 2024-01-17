3d0a345f39180e3fad70f7b62777b8f493db8c48
Jeeho Ahn <io@jcde.xyz>
2024-01-17 13:39:22 +0900

    Update dialog-copy.tsx

--- app/@modal/dialog-copy.tsx
+++ app/@modal/dialog-copy.tsx
@@ -57,6 +57,10 @@ const DialogContent = React.forwardRef<
           className={cn(
             "absolute right-4 top-4 rounded-sm opacity-70 ring-offset-background transition-opacity hover:opacity-100 focus:outline-none focus:ring-2 focus:ring-ring focus:ring-offset-2 disabled:pointer-events-none data-[state=open]:bg-accent data-[state=open]:text-muted-foreground"
           )}
+          onAnimationEnd={(e) => {
+          if (e.animationName === "exit") router.back();
+        }}
+          
         >
           <X className="h-4 w-4" />
           <span className="sr-only">Close</span>
